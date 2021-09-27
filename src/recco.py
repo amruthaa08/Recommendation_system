4fa84595545a94315bec21d43ff6e3aced2f2395
amruthaa08 <archeramruthaa@gmail.com>
2021-09-27 19:33:59 +0530

    code updates

--- src/recco.py
+++ src/recco.py
@@ -35,7 +35,7 @@ def get_recommendations(product):
     ind = indices[product]
     sim_scores = list(enumerate(cos_sim[ind]))
     sim_scores = sorted(sim_scores, key = lambda x:x[1], reverse=True)
-    product_indices = [i[0] for i in sim_scores[1:args.number]]
+    product_indices = [i[0] for i in sim_scores[1:(args.number + 1)]]
     print(products.iloc[product_indices])
 
 get_recommendations(args.prod_name)
